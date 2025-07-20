78027f3022a6740cc27e58fe2f5c72f2c72bb7af
Sujin Shin <shin369123@gmail.com>
2025-07-20 11:59:15 -0500

    Challenge #3b Multi-Node Broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"maps"
+	"slices"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
-	receivedMessages := make([]float64, 0)	// kept separate per each dest node
+	receivedMessages := make(map[float64] struct{}, 0)	// kept separate per each dest node
 	var topology map[string] any
 
 	n := maelstrom.NewNode()
@@ -21,11 +23,48 @@ func main() {
 			return err
 		}
 
-		// Record message in receivedMessages
-		if msgInt, ok := body["message"].(float64); ok {
-			receivedMessages = append(receivedMessages, msgInt)
-		} else {
-			return fmt.Errorf("unable to add message integer: message is type %T", body["message"])
+		msgInt, ok := body["message"].(float64);
+		if !ok {
+			return fmt.Errorf("unable to parse message as integer: message is type %T", body["message"])
+		}
+		
+		// Check if the message is new
+		if _, exists := receivedMessages[msgInt]; !exists {
+			// Record message in receivedMessages
+			receivedMessages[msgInt] = struct{}{}
+			
+			// Broadcast to neighbors not in the sender's network
+			broadcast_list := make(map[string] struct{}, 0)
+			if neighbors, ok := topology[msg.Dest].([]any); ok {
+				for _, node := range neighbors {
+					if node_name, ok := node.(string); ok {
+						broadcast_list[node_name] = struct{}{}
+					}
+				}
+			}
+			if neighbors, ok := topology[msg.Src].([]any); ok {
+				for _, node := range neighbors {
+					if node_name, ok := node.(string); ok {
+						delete(broadcast_list, node_name)
+					}
+				}
+			}
+			for node := range broadcast_list {
+				n.RPC(node, body, func(msg maelstrom.Message) error {
+					// Unmarshal message body
+					var body map[string] any
+					if err := json.Unmarshal(msg.Body, &body); err != nil {
+						return err
+					}
+
+					// Ensure reply from recipients
+					if msgtype, ok := body["type"].(string); (ok || msgtype != "broadcast_ok") {
+						return fmt.Errorf("did not get ok from %v", node)
+					}
+
+					return nil
+				})
+			}
 		}
 
 		// Create response body
@@ -39,7 +78,7 @@ func main() {
 	n.Handle("read", func(msg maelstrom.Message) error {
 		body := map[string] any {
 			"type": "read_ok",
-			"messages": receivedMessages,
+			"messages": slices.Collect(maps.Keys(receivedMessages)),
 		}
 
 		return n.Reply(msg, body)
